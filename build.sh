2a6d1df2d3dbc1d0e19e5ffd6ca4129b5c906ddf
serngawy <m.elserngawy@gmail.com>
2018-11-01 15:25:27 -0400

    Add ovs_driver and clean up the source code
    Signed-off-by: serngawy <m.elserngawy@gmail.com>

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+Update=$1
+
+export GOPATH=$PWD/vendor
+echo "GOPATH=" $GOPATH
+
+if [ ! -d "vendor/src" ] || [ "$Update" = "update" ]; then
+    mkdir -p bin
+    mkdir -p vendor/src
+
+    glide update
+    # glide is wired, create the src dir and move dependencies under it.
+    mkdir vendor/src
+    for dir in vendor/*; do
+      if [ "$dir" != "vendor/src" ]; then
+         cp -r $dir vendor/src;
+      fi;
+    done
+fi
+GOOS=linux go build -o bin/libovsdb
+
