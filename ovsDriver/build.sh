e03af43177c2b346a56eb9ca1c25f8de19d2e78e
serngawy <m.elserngawy@gmail.com>
2018-11-02 15:12:17 -0400

    Fix build script
    Signed-off-by: serngawy <m.elserngawy@gmail.com>

--- ovsDriver/build.sh
+++ ovsDriver/build.sh
@@ -2,21 +2,12 @@
 
 Update=$1
 
-export GOPATH=$PWD/vendor
 echo "GOPATH=" $GOPATH
 
 if [ ! -d "vendor/src" ] || [ "$Update" = "update" ]; then
-    mkdir -p ../bin
-    mkdir -p vendor/src
 
-    glide update
-    # glide is wired, create the src dir and move dependencies under it.
-    mkdir vendor/src
-    for dir in vendor/*; do
-      if [ "$dir" != "vendor/src" ]; then
-         cp -r $dir vendor/src;
-      fi;
-    done
+    glide update -v
+
 fi
-GOOS=linux go build -o ../bin/ovsDriver
+GOOS=linux go build -o $GOPATH/bin/ovsDriver
 
